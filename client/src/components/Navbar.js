0e2534b09f728b8f582c56ad6c1bb3afcc15a3d3
Md Azharuddin <stfuazzo@gmail.com>
2024-06-24 13:39:27 +0530

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiUpload, FiHome, FiInfo, FiLogIn } from 'react-icons/fi';
+import { FiUpload, FiHome, FiInfo, FiLogIn, FiMenu } from 'react-icons/fi';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="bg-gray-800 p-4 shadow-lg fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
@@ -10,28 +17,44 @@ const Navbar = () => {
           <FiUpload className="mr-2" />
           FileShare
         </Link>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="text-gray-300 hover:text-white flex items-center">
-              <FiHome className="mr-1" />
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="text-gray-300 hover:text-white flex items-center">
-              <FiInfo className="mr-1" />
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/signin" className="text-gray-300 hover:text-white flex items-center">
-              <FiLogIn className="mr-1" />
-              Sign In
-            </Link>
-          </li>
-        </ul>
+        <div className="menu-desktop hidden md:flex space-x-4">
+          <Link to="/" className="text-gray-300 hover:text-white flex items-center">
+            <FiHome className="mr-1" />
+            Home
+          </Link>
+          <Link to="/about" className="text-gray-300 hover:text-white flex items-center">
+            <FiInfo className="mr-1" />
+            About
+          </Link>
+          <Link to="/signin" className="text-gray-300 hover:text-white flex items-center">
+            <FiLogIn className="mr-1" />
+            Sign In
+          </Link>
+        </div>
+        <div className="menu-mobile md:hidden flex items-center">
+          <button onClick={toggleMenu} className="text-gray-300 hover:text-white">
+            <FiMenu size={24} />
+          </button>
+          {menuOpen && (
+            <div className="dropdown-menu bg-gray-800 shadow-lg rounded-lg absolute right-4 top-12 w-48">
+              <Link to="/" className="block px-4 py-2 text-gray-300 hover:text-white">
+                <FiHome className="mr-1" />
+                Home
+              </Link>
+              <Link to="/about" className="block px-4 py-2 text-gray-300 hover:text-white">
+                <FiInfo className="mr-1" />
+                About
+              </Link>
+              <Link to="/signin" className="block px-4 py-2 text-gray-300 hover:text-white">
+                <FiLogIn className="mr-1" />
+                Sign In
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
